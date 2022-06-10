08ab8ec2bcb14598133b305b8c830ad0c85fa1bc
chaitanya lohar <chaitanya.lohar@e-connectsolutions.com>
2022-06-10 17:51:37 +0530

    editing form created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 
 function Demo() {
   return (
+    <>
 <Navbar title="TextUtils Blog" homeText="Home" aboutText="About" contactText="Contact"  />
-
+<div className="container my-3">
+  <TextForm heading="Enter the text to analyze below" />
+  </div>
+</>
   );
 }
 
