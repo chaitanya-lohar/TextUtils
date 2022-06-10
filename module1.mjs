e28bc3038224fc382be830073c71c7e69e52a012
chaitanya lohar <chaitanya.lohar@e-connectsolutions.com>
2022-06-10 15:18:38 +0530

    module1 and module2 is just for understanding of export and import in files

---
+++ module1.mjs
@@ -0,0 +1,7 @@
+
+//import data from "./module2.mjs"
+import {a,b,d} from "./module2.mjs"
+//console.log(data);
+console.log(a)
+console.log(b)
+console.log(d)
---
+++ module2.mjs
@@ -0,0 +1,10 @@
+
+const a="Sam";
+const b="Manual";
+const c="Testing";
+const d="Getting";
+
+export default c;
+export {a}
+export {b}
+export {d}
