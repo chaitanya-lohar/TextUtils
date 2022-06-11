ec0c1d241a73cb3428116700759b4f1527129f7f
chaitanya lohar <chaitanya.lohar@e-connectsolutions.com>
2022-06-11 18:05:53 +0530

    new features added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,8 +2,8 @@
 import React,{useState} from 'react'
 
 export default function TextForm(props) {
-const[text,setText]=useState("Enter the text here...");
-  
+const[text,setText]=useState("");
+ 
   const HandleUpperClick=()=>{
    setText(text.toUpperCase());
   }
@@ -12,19 +12,67 @@ const[text,setText]=useState("Enter the text here...");
     setText(text.toLowerCase());
   }
 
+  const HandleClearText=()=>{
+    setText("");
+  }
+
+  const CopyToClipboard=()=>{
+    navigator.clipboard.writeText(text)
+  }
+
+  const ReverseWords=()=>{
+    let string=text.split(" ")
+    var newString=""
+    for(var i=parseInt(string.length)-1;i>=0;i--){
+       newString+=" "+string[i];
+    }
+    setText(newString.trimStart());
+  }
+
+  const ReverseCharacters=()=>{
+
+    let string=text;
+    let newString="";
+    for(let i=parseInt(string.length)-1;i>=0;i--){
+      newString+=string[i];
+    }
+    setText(newString);
+  }
+
+  const DownloadPDF=()=>{
+    
+   
+  }
+
   const HandleOnChange=(event)=>{
 
       setText(event.target.value);
+     
   }
 
  return (
      <>
      <h1>{props.heading}</h1>
-<div className="mb-3">
-  <textarea className="form-control" value={text} onChange={HandleOnChange} id="myBox" rows="10"></textarea>
-</div>
-<button className="btn btn-primary" onClick={HandleUpperClick}>Convert to Uppercase</button>
-<button className="btn btn-primary ms-2" onClick={HandleLowerClick}>Convert to Lowercase</button>
+      <div className=" container mb-3">
+        <textarea className="form-control" value={text} name="myBox" placeholder="Enter your text here..." onChange={HandleOnChange} id="myBox" rows="10"></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={HandleUpperClick}>UPPER CASE</button>
+      <button className="btn btn-primary ms-2" onClick={HandleLowerClick}>lower case</button>
+      <button className="btn btn-primary ms-2" onClick={HandleClearText}>Clear</button>
+      <button className="btn btn-primary ms-2" onClick={CopyToClipboard}>Copy to clipboard</button>
+      <button className="btn btn-primary ms-2" onClick={DownloadPDF}>Download PDF</button>
+      <button className="btn btn-primary ms-2" onClick={ReverseWords}>Reverse words</button>
+      <button className="btn btn-primary ms-2" onClick={ReverseCharacters}>Reverse Characters</button>
+
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p> {(text.split(" ")).length} words and {text.length} characters</p>
+        <p>{0.008*(text.split(" ")).length} minutes to read it.</p>
+      </div>
+      <h3>Preview</h3>
+      <div className=" container mb-3">
+        <textarea className="form-control" value={text} readOnly rows="10"></textarea>
+      </div>
 </>
   )
 }   
